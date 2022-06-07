a8f4a9d84feed9f09a8c24d5493258c98d74320c
Wiktoria Bryniowska <w.bryniowska@gmail.com>
2022-06-07 19:22:36 +0200

    Fix from 07-06

--- src/js/script.js
+++ src/js/script.js
@@ -103,12 +103,12 @@
       thisProduct.accordionTrigger.addEventListener("click", function (event) {
         event.preventDefault();
         let productActive = document.querySelector(
-          classNames.menuProduct.wrapperActive
+          ".product.active"
         );
         console.log("product active " + productActive);
         if (productActive && productActive != thisProduct.element) {
           console.log("prod act w if " + productActive);
-          productActive.element.classList.remove(
+          productActive.classList.remove(
             classNames.menuProduct.wrapperActive
           );
         }
@@ -149,24 +149,20 @@
       let price = thisProduct.data.price;
 
       // for every category (param)...
-      for(let paramId in thisProduct.data.params) {
+      for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
 
         // for every option in this category
-        for(let optionId in param.options) {
+        for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-
-          
-
         }
-      }      
+      }
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-      
     }
   }
 
